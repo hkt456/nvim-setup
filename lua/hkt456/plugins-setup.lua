655ab10267b08bbced721fad96913432ae9eb961
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-20 19:13:00 +0700

    added rust

--- lua/hkt456/plugins-setup.lua
+++ lua/hkt456/plugins-setup.lua
@@ -6,6 +6,8 @@
 --
 --   -- configure/enable gitsigns
 --   gitsigns.setup()-- auto install packer if not installed
+local cmp = require("cmp")
+
 local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
@@ -137,7 +139,47 @@ return packer.startup(function(use)
 		init = function()
 			vim.g.rustfmt_autosave = 1
 		end,
+	}, {
+		"simrat39/rust-tools.nvim",
+		ft = "rust",
+		dependencies = "neovim/nvim-lspconfig",
+		opts = function()
+			return require("hkt456.plugins.rust-tools.lua")
+		end,
+		config = function()
+			require("rust-tools").setup(_, opts)
+		end,
+	}, {
+		"mfussenegger/nvim-dap",
+		init = function()
+			require("core.utils").load_mappings("dap")
+		end,
+	}, {
+		"saecki/crates.nvim",
+		ft = { "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+			require("cmp").setup.buffer({
+				sources = { { name = "crates" } },
+			})
+			crates.show()
+			require("core.utils").load_mappings("crates")
+		end,
+	}, {
+		"hrsh7th/nvim-cmp",
+		opts = function()
+			local M = require("plugins.configs.cmp")
+			M.completion.completeopt = "menu,menuone,noselect"
+			M.mapping["<CR>"] = cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = false,
+			})
+			table.insert(M.sources, { name = "crates" })
+			return M
+		end,
 	})
+
 	--LaTex
 	use("lervag/vimtex")
 	if packer_bootstrap then
